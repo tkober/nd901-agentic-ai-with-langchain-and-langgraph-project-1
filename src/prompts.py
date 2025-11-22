74c79a1b6543320ca8e5749fc195deb30daab934
Thorsten Kober <thorsten.kober@gmail.com>
2025-11-22 22:25:20 +0100

    Task 3.1

--- src/prompts.py
+++ src/prompts.py
@@ -72,27 +72,29 @@ Guidelines:
 CALCULATION_SYSTEM_PROMPT = """"""
 
 
-# TODO: Finish the function to return the correct prompt based on intent type
+# Finish the function to return the correct prompt based on intent type
 # Refer to README.md Task 3.1 for details
 def get_chat_prompt_template(intent_type: str) -> ChatPromptTemplate:
     """
     Get the appropriate chat prompt template based on intent.
     """
     pass
-    # if intent_type == "qa":
-    #     system_prompt = QA_SYSTEM_PROMPT
-    # elif intent_type ==  # TODO:  Check the intent type value
-    #     system_prompt =  # TODO: Set system prompt to the correct value based on intent type
-    # elif intent_type ==  # TODO: Check the intent type value
-    # # TODO: Set system prompt to the correct value based on intent type
-    # else:
-    #     system_prompt = QA_SYSTEM_PROMPT  # Default fallback
+    if intent_type == "qa":
+        system_prompt = QA_SYSTEM_PROMPT
+    elif intent_type == "summarization":
+        system_prompt = SUMMARIZATION_SYSTEM_PROMPT
+    elif intent_type == "calculation":
+        system_prompt = CALCULATION_SYSTEM_PROMPT
+    else:
+        system_prompt = QA_SYSTEM_PROMPT  # Default fallback
 
-    # return ChatPromptTemplate.from_messages([
-    #     SystemMessagePromptTemplate.from_template(system_prompt),
-    #     MessagesPlaceholder("chat_history"),
-    #     HumanMessagePromptTemplate.from_template("{input}")
-    # ])
+    return ChatPromptTemplate.from_messages(
+        [
+            SystemMessagePromptTemplate.from_template(system_prompt),
+            MessagesPlaceholder("chat_history"),
+            HumanMessagePromptTemplate.from_template("{input}"),
+        ]
+    )
 
 
 # Memory Summary Prompt
